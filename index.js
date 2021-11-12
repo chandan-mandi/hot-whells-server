2241b0e4eb0a067d2d4807223cc8240d59cae022
chandan-mandi <itscoderchandan@gmail.com>
2021-11-13 04:34:32 +0530

    myOrders api ready

--- index.js
+++ index.js
@@ -20,20 +20,74 @@ async function run() {
     console.log('database connected');
     const database = client.db('Hot_Wheels');
     const carsCollection = database.collection('Available_Cars');
+    const bookingCollection = database.collection('Car_Booking')
+    const usersCollection = database.collection('User_Collection')
+    const reviewCollection = database.collection('Review_Collection')
     // GET ALL CARS DATA
-    app.get('/availableCars', async(req, res) => {
+    app.get('/availableCars', async (req, res) => {
       const cursor = carsCollection.find({});
       const result = await cursor.toArray();
       res.json(result)
     })
     // GET SINGLE CAR DATA
-    app.get('/availableCars/:id', async(req, res) => {
+    app.get('/availableCars/:id', async (req, res) => {
       const id = req.params.id;
       console.log('car id', id);
-      const query = {_id: ObjectId(id)}
+      const query = { _id: ObjectId(id) }
       const car = await carsCollection.findOne(query);
       res.json(car)
     })
+    // POST BOOKING API
+    app.post('/booking', async (req, res) => {
+      const booking = req.body;
+      console.log('hit the post api', booking);
+      const result = await bookingCollection.insertOne(booking)
+      res.json(result)
+    })
+    // GET ALL BOOKING API
+    app.get('/booking', async (req, res) => {
+      const cursor = bookingCollection.find({});
+      const result = await cursor.toArray();
+      res.json(result);
+    })
+    // GET SINGLE BOOKING DETAILS API
+    app.get('/booking/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) }
+      const booking = await bookingCollection.findOne(query)
+      res.send(booking)
+    })
+    // GET MY BOOKING
+    app.get('/myBooking/:email', async(req, res) => {
+      const email = req.params.email;
+      const query = {email : email};
+      const myBooking = await bookingCollection.find(query).toArray();
+      res.json(myBooking);
+    })
+    // UPDATE SINGLE BOOKING DETAILS API
+    app.patch('/booking/:id', async (req, res) => {
+      const id = req.params.id;
+      const updateBooking = req.body;
+      const filter = { _id: ObjectId(id) }
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          status: updateBooking.status
+        },
+      };
+      const result = await bookingCollection.updateOne(filter, updateDoc, options)
+      res.json(result)
+    })
+    // POST METHOD USER DETAILS
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      res.json(result)
+      console.log(result);
+    })
+    // ADDED ADMIN ROLE
+    // app.put('/users/admin')
+    // 
   }
   catch {
     // await client.close();
